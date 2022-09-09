0f8f4bfb7c58402faeceb59c1a871b2b1d5d404e
agustina gomez <agustinagxmez@gmail.com>
2022-09-09 17:32:35 -0300

    initial commit

---
+++ api/index.js
@@ -0,0 +1,13 @@
+
+const server = require('./src/app.js');   
+const { conn } = require('./src/db.js');
+
+// Syncing all the models at once.
+// {force: true}
+conn.sync({force: false}).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  }, console.log(`Server listening on port ${process.env.PORT}.`));
+});
+
+
