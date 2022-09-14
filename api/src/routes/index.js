ad4abc1a25f212c59d6c3710d0e6dbbd9c46040e
Luis Ballesteros <luisballesteros96@hotmail.com>
2022-09-14 01:34:42 -0500

    modulation in routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,5 +1,9 @@
-const { Users, Posts } = require('../db');
-const { Router } = require('express');
+const { Users, Posts } = require("../db");
+const { Router } = require("express");
+const createUser = require("../Controller/createUser");
+const updateUser = require("../Controller/updateUser");
+const createPost = require("../Controller/createPost");
+const updatePost = require("../Controller/updatePost");
 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
@@ -8,77 +12,82 @@ const router = Router();
 
 // router.use()
 
-router.post('/users', async (req, res) => {
-    const { name, role, email, password, username, avatar } = req.body;
-    try {
-        let user = await Users.create({
-            name, 
-            role, 
-            email, 
-            password, 
-            username, 
-            avatar
-        })
-        res.json(user);
-    } catch (err) {
-        res.status(404).send(err)
-    }
-});
+// router.post("/users", async (req, res) => {
+//   const { name, role, email, password, username, avatar } = req.body;
+//   try {
+//     let user = await Users.create({
+//       name,
+//       role,
+//       email,
+//       password,
+//       username,
+//       avatar,
+//     });
+//     res.json(user);
+//   } catch (err) {
+//     res.status(404).send(err);
+//   }
+// });
 
-router.put('/users/:nickname', async (req, res) => {
-    const { nickname } = req.params;
-    const { name, username, avatar } = req.body;
-    try {
-        let user = await Users.findOne({ where: {username: nickname} });
-        user.update({
-            name,
-            username,
-            avatar
-        })
-        await user.save();
-        res.json(user);
-    } catch (err) {
-        res.status(404).send(err);
-    }
-});
+router.post("/users", createUser);
 
+// router.put("/users/:nickname", async (req, res) => {
+//   const { nickname } = req.params;
+//   const { name, username, avatar } = req.body;
+//   try {
+//     let user = await Users.findOne({ where: { username: nickname } });
+//     user.update({
+//       name,
+//       username,
+//       avatar,
+//     });
+//     await user.save();
+//     res.json(user);
+//   } catch (err) {
+//     res.status(404).send(err);
+//   }
+// });
 
-router.post('/posts', async (req, res) => {
-    const { description, title, content, email } = req.body;
-    try {
-        let post = await Posts.create({
-            description,
-            title,
-            content
-        });
-        let user = await Users.findOne({ where: {email} });
-        post.setUser(user);
-        res.json(post);
-    } catch (err) {
-        res.status(404).send(err);
-    }
-});
+router.put("/users/:nickname", updateUser);
 
-router.put('/posts/:id', async (req, res) => {
-    const { id } = req.params;
-    const { description, title, content } = req.body;
-    try {
-        let post = await Posts.findOne({ where: {id} });
-        post.update({
-            description,
-            title,
-            content,
-        })
-        await post.save();
-        res.json(post);
-    } catch (err) {
-        res.status(404).send(err);
-    }
-});
+// router.post("/posts", async (req, res) => {
+//   const { description, title, content, email } = req.body;
+//   try {
+//     let post = await Posts.create({
+//       description,
+//       title,
+//       content,
+//     });
+//     let user = await Users.findOne({ where: { email } });
+//     post.setUser(user);
+//     res.json(post);
+//   } catch (err) {
+//     res.status(404).send(err);
+//   }
+// });
 
+router.post("/posts", createPost);
+
+// router.put("/posts/:id", async (req, res) => {
+//   const { id } = req.params;
+//   const { description, title, content } = req.body;
+//   try {
+//     let post = await Posts.findOne({ where: { id } });
+//     post.update({
+//       description,
+//       title,
+//       content,
+//     });
+//     await post.save();
+//     res.json(post);
+//   } catch (err) {
+//     res.status(404).send(err);
+//   }
+// });
+
+router.put("/posts/:id", updatePost);
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
 module.exports = router;
-
