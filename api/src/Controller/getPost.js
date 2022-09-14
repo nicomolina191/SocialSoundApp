3504b7cf855686245abf1c6c05885095fa643ee3
Luis Ballesteros <luisballesteros96@hotmail.com>
2022-09-14 11:57:34 -0500

    get routes

---
+++ api/src/Controller/getPost.js
@@ -0,0 +1,13 @@
+const { Users, Posts } = require("../db");
+
+const getPost = async (req, res) => {
+  try {
+    const findPost = await Posts.findAll();
+
+    res.send(findPost);
+    return findPost;
+  } catch (error) {
+    res.send(error);
+  }
+};
+module.exports = getPost;
