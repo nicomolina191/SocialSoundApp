7613decfce280e8b106a2d29a8e82b6b2d5fc00d
Santino912 <santinoscolamieri@yahoo.com.ar>
2022-09-16 11:51:49 -0300

    register login css

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Explore from "./components/Explore/Explore";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import Explore from "./components/Explore";
 import { AuthProvider } from "./context";
 import ProtectedRoute from "./context/ProtectedRoute";
 import "./App.css";
-import Landing from "./components/landing/Landing";
-import Post from "./components/post/Post";
 function App() {
   return (
     <React.StrictMode>
