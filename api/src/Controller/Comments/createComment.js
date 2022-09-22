d343a877c809f5e890004efbf6168174448a3ad4
Luis Ballesteros <luisballesteros96@hotmail.com>
2022-09-22 17:22:24 -0500

    fix error with routes and override use of censored words in comments

--- api/src/Controller/Comments/createComment.js
+++ api/src/Controller/Comments/createComment.js
@@ -1,17 +1,24 @@
-const { Users, Posts, Comments } = require('../../db.js');
+const { Users, Posts, Comments } = require("../../db.js");
+const badWords = require("./BadWords.js");
 
 const createComment = async (req, res) => {
   const { content, idPost, idUser } = req.body;
   try {
-    const addComent = await Comments.create({
-      content,
-    });
+    let censoredWords = badWords;
 
-    const user = await Users.findByPk(idUser);
-    await addComent.setUser(user);
-    const post = await Posts.findByPk(idPost);
-    await addComent.setPost(post);
-    res.json(addComent);
+    const sensitiveWords = censoredWords.find((ele) => content.includes(ele));
+    if (sensitiveWords) {
+      res.send("Your comment is not appropriate, please do it again");
+    } else {
+      const addComent = await Comments.create({
+        content,
+      });
+      const user = await Users.findByPk(idUser);
+      await addComent.setUser(user);
+      const post = await Posts.findByPk(idPost);
+      await addComent.setPost(post);
+      res.json(addComent);
+    }
   } catch (error) {
     res.json(error);
   }
