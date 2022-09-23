c7003f09473cfec0e3ae9d2e47a1bdb7a1f97288
fervicente8 <fferminvicente@gmail.com>
2022-09-23 14:19:14 -0300

    d

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -24,7 +24,6 @@ const setNotiWatched = require("../Controller/Notifications/setNotiWatched");
 const getUserByIdAdmin = require("../Controller/Users/getUserByIdAdmin.js");
 const getUsersAdmin = require("../Controller/Users/getUserAdmin.js");
 const getUserByIdGoogleAdmin = require("../Controller/Users/getUserByIdGoogleAdmin.js");
-const getUserByIdAdmin = require("../Controller/Users/getUserByIdAdmin.js");
 const getLikesByPostId = require('../Controller/Likes/getLikesByPostId.js');
 const getPostById = require("../Controller/Posts/getPostById.js");
 const getLikesByPostandUserId = require('../Controller/Likes/getLikesByPostandUserId.js');
