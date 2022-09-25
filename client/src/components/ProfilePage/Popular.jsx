ef47d398c99d4688f3b9d3bb6e782b2224f803fc
fervicente8 <fferminvicente@gmail.com>
2022-09-24 21:57:27 -0300

    e

--- client/src/components/ProfilePage/Popular.jsx
+++ client/src/components/ProfilePage/Popular.jsx
@@ -7,8 +7,9 @@ import portada from "./Play.png";
 const Popular = ({ id }) => {
   const dispatch = useDispatch();
   const allPosts = useSelector((state) => state.posts.possListAll);
-  const popularPosts = Array.isArray(allPosts) ? allPosts.filter((post) => post.userId === id).slice(0, 5) : []
-    
+  const popularPosts = allPosts
+    .filter((post) => post.userId === id)
+    .slice(0, 5);
 
   useEffect(() => {
     dispatch(getPostByTime("popu"));
@@ -25,8 +26,8 @@ const Popular = ({ id }) => {
                 <div className={styles.songFirstHalfIndex}>
                   <p>{index + 1}</p>
                 </div>
-                  <img src={portada} alt="" />
-                  <p>{post.title}</p>
+                <img src={portada} alt="" />
+                <p>{post.title}</p>
               </div>
               <div className={styles.songSecondHalf}>
                 <p>6,145,232</p>
