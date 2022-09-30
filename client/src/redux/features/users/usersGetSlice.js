8d380b6d15c04116a799511e9820f452c34a8c19
fervicente8 <fferminvicente@gmail.com>
2022-09-30 15:30:07 -0300

    Vaciar estado user al cambiar perfil, creo q anda nose no me doy cuenta

--- client/src/redux/features/users/usersGetSlice.js
+++ client/src/redux/features/users/usersGetSlice.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { addUsers, deleteUsers, getUserError, getUserStart, getUserSuccess, updateUsers, getById, getByFirebaseId, getUpdatePremium, getLikes, setGenres, getNotifications, createNotification, watchedNotification, disabledNotification  } from "./usersSlice";
+import { addUsers, deleteUsers, getUserError, getUserStart, getUserSuccess, updateUsers, getById, getByFirebaseId, getUpdatePremium, getLikes, setGenres, getNotifications, createNotification, watchedNotification, disabledNotification, cleanUser  } from "./usersSlice";
 
 
 //obtener los users
@@ -75,6 +75,17 @@ export const getUserById = (id) => {
     }
   }
 }
+
+export const cleanUserState = () => {
+  return async (dispatch) => {
+    try {
+      dispatch(cleanUser())
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
 export const getUserByFirebaseId = (id) => {
   return async (dispatch) => {
     try {
