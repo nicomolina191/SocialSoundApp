f90a7c7a4958f1db2d007b58416197621d289ec3
Santino912 <santinoscolamieri@yahoo.com.ar>
2022-09-30 21:37:15 -0300

    admin post reports components

--- client/src/components/admin/reportUsers/ReportUsers.jsx
+++ client/src/components/admin/reportUsers/ReportUsers.jsx
@@ -1,13 +1,19 @@
 import { Avatar } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import style from "./reportUsers.module.css"
 
-const ReportUsers = ({idUser, idPost }) => {
+const ReportUsers = ({data: {title, content, postId, userId, user:{name, email}}}) => {
   return (
     <Box className={style.containerAll}>
         <Box className={style.reporterDiv}>
-            <Avatar/><h2>Dawad</h2>
+            <Avatar alt={"d"}/>
+            <Link to={userId}><h6>{email}</h6></Link>
+            <h1>Reason: {title}</h1>
+            <Box className={style.detailContainer}>
+            <h3>Detail: {content}</h3>
+            </Box>
         </Box>
     </Box>
   )
