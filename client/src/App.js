087fb4d71977eb1b627440d4ae67d16870d88c16
alanrflores <alan.flores640@gmail.com>
2022-10-03 13:20:42 -0300

    arreglo de errores

--- client/src/App.js
+++ client/src/App.js
@@ -74,7 +74,7 @@ function App() {
               </ProtectedRoute>
             }
           />
-          <Route path="/home/sucess" element={
+          <Route path="/home/success" element={
             <ProtectedRoute>
               <Success />
             </ProtectedRoute>
